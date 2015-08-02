f0fe95af7717096ed2dcd7a580452d266f1d3a28
basti2342 <basti@randomprojects.de>
2015-08-02 20:37:26 +0200

    internal area introduced

--- club_veb/club_veb/urls.py
+++ club_veb/club_veb/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.views.generic.base import TemplateView
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
@@ -8,7 +9,21 @@ urlpatterns = [
     url(r'^programm/$', 'club_veb.views.programm', name='programm'),
     url(r'^kontakt/$', 'club_veb.views.kontakt', name='kontakt'),
     url(r'^galerie/$', 'club_veb.views.galerie', name='galerie'),
-    url(r'^impressum/$', 'club_veb.views.impressum', name='impressum'),
-    url(r'^planung/$', 'club_veb.views.planung', name='planung'),
-    url(r'', 'club_veb.views.zentrale', name='zentrale'),
+    url(r'^impressum/$', TemplateView.as_view(template_name='impressum.html'),
+        name='impressum'),
+    url(r'^intern/$', 'club_veb.views.intern_uebersicht',
+        name='intern_uebersicht'),
+    url(r'^intern/booking/$', 'club_veb.views.intern_booking',
+        name='intern_booking'),
+    url(r'^intern/schichtplan/$', 'club_veb.views.intern_schichtplan',
+        name='intern_schichtplan'),
+    url(r'^intern/todo$', 'club_veb.views.intern_todo', name='intern_todo'),
+    url(r'^intern/clubtreffen/$', 'club_veb.views.intern_clubtreffen',
+        name='intern_clubtreffen'),
+    url(r'^intern/benutzer/$', 'club_veb.views.intern_benutzer',
+        name='intern_benutzer'),
+    url(r'^intern/mail/$', 'club_veb.views.intern_mail', name='intern_mail'),
+    url(r'^intern/kollektiv/$', 'club_veb.views.intern_kollektiv',
+        name='intern_kollektiv'),
+    url(r'^$', 'club_veb.views.zentrale', name='zentrale'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # devel only
--- club_veb/club_veb/views.py
+++ club_veb/club_veb/views.py
@@ -21,19 +21,43 @@ def galerie(request):
     return render(request, 'galerie.html')
 
 
-def impressum(request):
-    return render(request, 'impressum.html')
+def intern_uebersicht(request):
+    return render(request, 'intern/uebersicht.html')
 
 
-def planung(request):
-    if request.method == "POST":
+def intern_booking(request):
+    if request.method == 'POST':
         booking = BookingForm(request.POST, request.FILES)
         if booking.is_valid():
             booking.save()
             return HttpResponseRedirect(
-                reverse("club_veb.views.zentrale")
+                reverse('club_veb.views.zentrale')
             )
     else:
         booking = BookingForm()
 
-    return render(request, 'planung.html', {'booking': booking})
+    return render(request, 'intern/booking.html', {'booking': booking})
+
+
+def intern_schichtplan(request):
+    return render(request, 'intern/schichtplan.html')
+
+
+def intern_todo(request):
+    return render(request, 'intern/todo.html')
+
+
+def intern_clubtreffen(request):
+    return render(request, 'intern/clubtreffen.html')
+
+
+def intern_benutzer(request):
+    return render(request, 'intern/benutzer.html')
+
+
+def intern_mail(request):
+    return render(request, 'intern/mail.html')
+
+
+def intern_kollektiv(request):
+    return render(request, 'intern/kollektiv.html')

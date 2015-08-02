1f7f2804b99f6078dd8eede92197b1abbe98a29c
basti2342 <basti@randomprojects.de>
2015-08-02 15:09:23 +0200

    event editor: show full responsible name

--- club_veb/club_veb/forms.py
+++ club_veb/club_veb/forms.py
@@ -1,9 +1,21 @@
-from django.forms import ModelForm, Textarea
+from django.forms import ModelForm, Textarea, ModelChoiceField
 from datetimewidget.widgets import DateWidget
+
 from club_veb.models import Booking
+from django.contrib.auth.models import User
+
+
+class UserFullnameChoiceField(ModelChoiceField):
+    def label_from_instance(self, obj):
+        if obj.get_full_name() == '':
+            return obj.username
+        else:
+            return obj.get_full_name()
 
 
 class BookingForm(ModelForm):
+    responsible = UserFullnameChoiceField(queryset=User.objects.all())
+
     def __init__(self, *args, **kwargs):
         super(BookingForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
